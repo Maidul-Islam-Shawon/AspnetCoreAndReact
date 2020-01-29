296dcf44b40753067267038e2d7b0d38bfc9483e
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-01-29 18:29:40 +0600

    StudentController, AppDbContext and Migration Added.

---
+++ Samples/WebApiApp/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private AppDbContext _context;
+        private ILogger logger;
+
+        public StudentsController(AppDbContext context, ILoggerFactory factory)
+        {
+            _context = context;
+            logger = factory.CreateLogger<StudentsController>();
+        }
+
+
+        [HttpGet]
+        [Route("GetStudents")]
+        public IActionResult GetStudents(int skip=0, int take=10, string orderBy="Name", bool isAssending=true)
+        {
+            IQueryable<Student> dbStudents = _context.Students.AsQueryable();
+
+            if (orderBy == "Name")
+            {
+                dbStudents = isAssending ? dbStudents.OrderBy(x => x.Name).AsQueryable() :
+                                        dbStudents.OrderByDescending(x => x.Name).AsQueryable();
+            }
+
+            if (orderBy == "Phone")
+            {
+                dbStudents = isAssending ? dbStudents.OrderBy(x => x.Phone).AsQueryable() :
+                                        dbStudents.OrderByDescending(x => x.Phone).AsQueryable();
+            }
+
+            dbStudents = dbStudents.Skip(skip).Take(take).AsQueryable();
+            List<Student> students = dbStudents.ToList();
+            //students.Add(new Student() { Id= 1, Name="abc", Phone="123" });
+            //students.Add(new Student() { Id = 2, Name = "efg", Phone = "456" });
+
+            return Ok(students);
+        }
+
+        [HttpPost]
+        [Route("SaveStudent")]
+        public IActionResult SaveStudent([FromBody] Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        [Route("DeleteStudnet")]
+        public IActionResult DeleteStudnet(int id)
+        {
+            return Ok(id);
+        }
+
+    }
+}
--- Samples/WebApiApp/Startup.cs
+++ Samples/WebApiApp/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiApp.Models;
 
 namespace WebApiApp
 {
@@ -25,6 +27,9 @@ namespace WebApiApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var ConnectionString = this.Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options=> { options.UseSqlServer(ConnectionString); });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
